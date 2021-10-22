8d4889d50013fc63138713fb0e7d4bb0497bbb40
sandrazfd <sandrazfd@hotmail.com>
2021-10-22 16:35:09 -0300

    React -frontend

---
+++ src/components/LoginBox/index.tsx
@@ -0,0 +1,32 @@
+import styles from './styles.module.scss';
+import { VscGithubInverted } from 'react-icons/vsc'
+import { useContext } from 'react';
+import { AuthContext } from '../../context/auth';
+
+
+type AuthResponse = {
+    token: string;
+    user: {
+        id: string;
+        avatar_url: string;
+        name: string;
+        login: string;
+    }
+}
+
+export function LoginBox() {
+    const { signInUrl } = useContext(AuthContext);
+
+
+
+    return (
+        <div className={styles.loginBoxWrapper}>
+            <strong>Entre e compartilhe sua mensagem</strong>
+            <a href={signInUrl} className={styles.signInWithGithub}>
+                <VscGithubInverted size="24" />
+                Entrar com github
+            </a>
+        </div>
+
+    )
+}
